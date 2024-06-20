55e41ef1bcd1212f90a626a4c022db4004616c2b
dipikabajracharya <dipikabajracharya57@gmail.com>
2024-06-20 15:50:33 +0545

    feat:added 404 page

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+import NotFound from "../components/NotFound";
+
+const PageNotFound = () => {
+  return (
+    <>
+      <NavBar />
+      <NotFound />
+    </>
+  );
+};
+export default PageNotFound;
